ba740c2b887a060b5be27c5385c7797b54b5da13
Philip Schiff <philipschiff@gmail.com>
2022-03-07 00:54:13 -0600

    Add Board.board and Board.print_board

--- board.py
+++ board.py
@@ -2,10 +2,33 @@ class Board:
     """
     Put description of the Board class here
     """
+
+    STARTING_POSITION = [
+        ["R", "N", "B", "Q", "K", "B", "N", "R"],
+        ["P", "P", "P", "P", "P", "P", "P", "P"],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["p", "p", "p", "p", "p", "p", "p", "p"],
+        ["r", "n", "b", "q", "k", "b", "n", "r"]
+    ]
+
     def __init__(self):
-        # TODO: convert to standard starting position
-        self.board = [[None, None, None, None, None, None, None, None] for i in range(8)]
+        self.board = Board.STARTING_POSITION
+
 
     def print_board(self):
         # replace body with how you want your board printed
         print("Printing the Board")
+        board_string = ""
+        for row in self.board:
+            for square in row:
+                board_string += "|"
+                if square:
+                    board_string += square
+                else:
+                    board_string += " "
+            board_string += "|\n"
+
+        print(board_string)
